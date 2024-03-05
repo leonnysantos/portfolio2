a143fb19ff78311554d1dfebdfee58da42ecf5b0
leonnysantos <leonnypat@gmail.com>
2024-03-04 23:41:23 -0300

    feat: create navbar and animated background

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,17 +1,23 @@
-import { Button, Container, Grid, Typography, styled } from "@mui/material"
+import { Box, Container, Grid, Typography, styled } from "@mui/material"
 import avatar from "../../../../assets/images/avatar.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import theme from "../../../../theme";
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
-    const StyledHero = styled("div")(() => ({
-        backgroundColor: "black",
-        height: "100vh"
+    const StyledHero = styled("div")(({ }) => ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center"
     }))
 
-    const StyledImage = styled("img")(() => ({
-        width: "100%",
-        borderRadius: "50%"
+    const StyledImage = styled("img")(({ theme }) => ({
+        width: "80%",
+        borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
     }))
 
     return (
@@ -19,24 +25,35 @@ const Hero = () => {
             <StyledHero>
                 <Container maxWidth="lg">
                     <Grid container spacing={2}>
-                        <Grid item xs={12} md={4}>
-                            <StyledImage src={avatar} />
+                        <Grid item xs={12} md={5}>
+                            <Box position="relative">
+                                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                    <AnimatedBackground />
+                                </Box>
+                                <Box position="relative" textAlign="center">
+                                    <StyledImage src={avatar} />
+                                </Box>
+                            </Box>
                         </Grid>
-                        <Grid item xs={12} md={8}>
-                            <Typography color="primary" variant="h1" textAlign="center">Leonny Santos</Typography>
-                            <Typography color="primary" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
-                            <Grid container display="flex" justifyContent="center">
+                        <Grid item xs={12} md={7}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Leonny Santos</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
+                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                                 <Grid item xs={12} md={6} display="flex" justifyContent="center">
-                                    <Button>
+                                    <StyledButton>
                                         <DownloadIcon />
-                                        Download CV
-                                    </Button>
+                                        <Typography>
+                                            Download CV
+                                        </Typography>
+                                    </StyledButton>
                                 </Grid>
                                 <Grid item xs={12} md={6} display="flex" justifyContent="center">
-                                    <Button>
+                                    <StyledButton>
                                         <MailOutlineIcon />
-                                        Contact Me
-                                    </Button>
+                                        <Typography>
+                                            Contact Me
+                                        </Typography>
+                                    </StyledButton>
                                 </Grid>
                             </Grid>
                         </Grid>
